fbbc1a26911feb7dc70da97b5c752c287d915565
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-10-09 14:28:46 -0300

    Corrigindo erro postagem

--- BlogPessoal/Model/Auditable.cs
+++ BlogPessoal/Model/Auditable.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogPessoal.Model
 {
     public class Auditable
     {
+        [Column(TypeName = "datetimeoffset")]
         public DateTimeOffset? Data { get; set; }
 
     }
